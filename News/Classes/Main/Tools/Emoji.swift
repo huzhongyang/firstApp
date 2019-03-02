f0645fbe5fdd3accf1fc25095c9a82da1869b3c7
zy H <hzy18023@gmail.com>
2019-03-02 14:59:45 +0800

    实现高亮显示@用户名

--- News/Classes/Main/Tools/Emoji.swift
+++ News/Classes/Main/Tools/Emoji.swift
@@ -30,8 +30,30 @@ struct EmojiManger {
     
     /// 显示 emoji 表情
     func showEmoji(content: String, font: UIFont) -> NSMutableAttributedString {
+        
         // 将 content 转成 attributedString
         let attributedString = NSMutableAttributedString(string: content)
+        
+        // 用户名匹配规则
+        let userPatern = "@.*? "
+        // 创建正则表达式对象，匹配用户名
+        let userRegex = try! NSRegularExpression(pattern: userPatern, options: [])
+        // 开始匹配用户名，并返回结果
+        let userResults = userRegex.matches(in: content, options: [], range: NSRange(location: 0, length: attributedString.length))
+        if userResults.count != 0 {
+            for result in userResults {
+                // 从 content 中取出用户名
+                let userName = (content as NSString).substring(with: result.range)
+                let attributedUserName = NSMutableAttributedString(string: userName)
+                // 设置颜色
+                attributedUserName.setAttributes([.foregroundColor: UIColor(r: 51, g: 153, b: 255)], range: NSRange(location: 0, length: attributedUserName.length))
+                // 替换
+                attributedString.replaceCharacters(in: result.range, with: attributedUserName)
+            }
+        }
+        
+        
+        
         // emoji 表情的正则表达式
         let emojiPattern = "\\[.*?\\]"
         // 创建正则表达式对象，匹配 emoji 表情
